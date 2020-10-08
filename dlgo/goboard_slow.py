ad1a0416838181ca3cd86a845502e33e87b89728
Pavan Kunchala <pavankuchalapk@gmail.com>
2020-10-08 15:40:12 +0530

    Update goboard_slow.py

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -59,6 +59,90 @@ class GoString():
             self.color == other.color and \
             self.stones == other.stones and \
             self.liberties == other.liberties
+    
+
+class Board():
+    """
+    A board is initialized as an empty grid with the specified number of rows and columns.
+    """
+
+    def __init__(self,num_rows, num_cols):
+        self.num_rows = num_cols
+        self.num_cols =  num_cols
+        self.grid = {}
+    
+    def place_stone(self,player,point):
+        
+        assert self.is_on_grid(point)
+        assert self._grid.get(point) is None
+        adjacent_same_color = []
+        adjacent_opposite_color = []
+        liberties = []
+
+        for neighbor in point.neighbors():
+            if not self.is_on_grid(neighbor):
+                continue
+            neighbor_string= self._grid.get(neighbor)
+            if neighbor_string is None:
+                liberties.append(neighbor)
+            elif neighbor_string.color == player:
+
+                if neighbor_string not in adjacent_same_color:
+                    adjacent_same_color.append(neighbor_string)
+            else:
+                if neighbor_string not in adjacent_opposite_color:
+                    adjacent_opposite_color.append(neighbor_string)
+
+        new_string = GoString(player,[point],liberties)
+
+        #Merge any adjacent strings of the same color.
+        for same_color_string in adjacent_same_color:  
+            new_string = new_string.merged_with(same_color_string)
+        for new_string_point in new_string.stones:
+            self._grid[new_string_point] = new_string
+        #Reduce liberties of any adjacent strings of the opposite color.
+        for other_color_string in adjacent_opposite_color:  
+            other_color_string.remove_liberty(point)
+        #If any opposite­color strings now have zero liberties, remove them.
+        for other_color_string in adjacent_opposite_color: 
+            if other_color_string.num_liberties == 0:
+                self._remove_string(other_color_string)
+
+    def is_on_grid(self,point):
+        return 1 <= point.row <= self.num_rows and   1 <= point.col <= self.num_cols
+
+    """
+    Returns the content of a point on the board: a Player if a stone is on that point, or else None
+    """
+    def get(self,point):
+        string = self._grid.get(point)
+        if string is None:
+            return None
+        return string.color
+
+    """
+    Returns the entire string of stones at a point: a GoString if a stone is on that point, or else None
+    """
+    def get_go_string(self,point):
+        string = self._grid.get(point)
+        if string is None:
+
+            return None
+        return string
+
+
+        
+
+
+
+        
+
+
+
+
+       
+
+    
 
 
     
