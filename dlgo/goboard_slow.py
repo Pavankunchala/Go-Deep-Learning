49cf0cc95d746f07b52bef5b80b885faedbad622
Pavan Kunchala <pavankuchalapk@gmail.com>
2020-10-08 16:56:14 +0530

    class for Game State
    I have created a class for checking the current game state of the board

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -143,7 +143,53 @@ class Board():
 #creating A GAME STATE
 
 class GameState():
+
+    def __init__(self, board,next_player,previous,move):
+        self.board = board
+        self.next_player =  next_player
+        self.previous_state = previous
+        self.last_move = move
+
+    def apply_move(self,move):
+        if move.is_play:
+            next_board = copy.deepcopy(self.board)
+            next_board.place_stone(self.next_player,move.point)
+
+        else :
+            next_board = self.board
+        return GameState(next_board, self.next_player.other, self, move)
+
+    @classmethod
+    def new_game(cls,board_size):
+        if isinstance(board_size,int):
+            board_size = (board_size,board_size)
+
+            board = Board(*board_size)
+        return GameState(board,Player.black,None,None)
     
+    def is_over(self):
+        if self.last_move is None:
+            return False
+
+        if self.last_move.is_resign:
+            return True
+
+        second_last_move =self.previous_state.last_move 
+        if second_last_move is None:
+            return False
+        return self.last_move.is_pass and second_last_move.is_pass
+        
+
+
+
+
+    
+    
+
+
+       
+
+       
 
 
         
