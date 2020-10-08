a1b43a143f6c79809f61701caf4e6d9f7b21a59f
Pavan Kunchala <pavankuchalapk@gmail.com>
2020-10-08 14:56:33 +0530

    Update goboard_slow.py

--- dlgo/goboard_slow.py
+++ dlgo/goboard_slow.py
@@ -24,7 +24,47 @@ class Move():
     @classmethod
     def resign(cls):
         return Move(is_resign=True)
-        
+
+# Go strings are a chain of connected stones of the same color.
+class GoString():
+
+    def __init__(self,color, stones ,liberties):
+        self.color = color
+        self.stones= set(stones)
+        self.liberties = set(liberties)
+
+    #Removing a libertiy
+    def remove_libertiy(self,point):
+        self.liberties.remove(point)
+    #adding a libertiy
+    def add_libertiy(self,point):
+        self.liberties.add(point)
+
+    def merged_with(self,go_string):
+        assert go_string.color == self.color
+        combined_stones = self.stones | go_string.stones
+
+        return GoString(
+            self.color,
+            combined_stones,
+            (self.liberties | go_string.liberties) - combined_stones
+        )
+    @property
+    def num_liberties(self):
+        return len(self.liberties)
+
+    def __eq__(self, other):
+
+        return isinstance(other, GoString) and \
+            self.color == other.color and \
+            self.stones == other.stones and \
+            self.liberties == other.liberties
+
+
+    
+
+
+       
 
     
 
